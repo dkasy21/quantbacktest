07e3cd80969c2936f1b403597febf38694a0acb5
dkasy21 <danylobjj@gmail.com>
2026-07-02 20:00:02 +0200

    fix: use experimental.serverComponentsExternalPackages for yahoo-finance2 (Next.js 14 compat)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['yahoo-finance2'],
-  webpack: (config) => {
-    config.resolve.alias = config.resolve.alias || {};
-    config.resolve.alias['@std/testing/mock'] = false;
-    config.resolve.alias['@std/testing'] = false;
-    return config;
+  experimental: {
+    serverComponentsExternalPackages: ['yahoo-finance2'],
   },
 };
 
