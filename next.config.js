cae73ec6ad1a0f832c7df1a3b7b8d80e0f0af3a8
dkasy21 <danylobjj@gmail.com>
2026-07-03 14:27:38 +0200

    fix: add webpack IgnorePlugin for @std/* inside yahoo-finance2

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['yahoo-finance2'],
   },
+  webpack: (config, { webpack: wp }) => {
+    // Safety net: ignore any Deno stdlib imports inside yahoo-finance2 ESM tree
+    config.plugins.push(
+      new wp.IgnorePlugin({
+        resourceRegExp: /^@std\//,
+        contextRegExp: /yahoo-finance2/,
+      })
+    );
+    return config;
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
